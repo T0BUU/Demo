1409052c99729ab8c22a7948eaa34b28411d08fb
Otto von Boehm <otto.vonboehm@aalto.fi>
2018-01-30 16:31:34 +0200

    Reworked UI; changed pager to navigation drawer. Drawer contains profile and some vital buttons (login). Drawer Java code is in MainActivity. Added drawable camera_floating_button, finnair emblem, profile icon, cards logo and money logo. Layout folder has drawer outlook xml files. And at last created dimens.xml to values folder, and added few more strings to strings.xml

--- app/src/main/java/com/finnair/gamifiedpartnermap/MainActivity.java
+++ app/src/main/java/com/finnair/gamifiedpartnermap/MainActivity.java
@@ -2,11 +2,20 @@ package com.finnair.gamifiedpartnermap;
 
 import android.location.Location;
 import android.os.Bundle;
-import android.support.design.widget.TabLayout;
-import android.support.v4.view.ViewPager;
+import android.support.design.widget.NavigationView;
+import android.support.v4.app.Fragment;
+import android.support.v4.view.GravityCompat;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.support.v4.app.FragmentManager;
 import android.util.Log;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageButton;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.google.android.gms.maps.GoogleMap;
@@ -14,51 +23,103 @@ import com.google.android.gms.maps.GoogleMap;
 /**
  * Created by ala-hazla on 16.12.2017.
  */
+//Modified by Otto on 11.1.2018, added drawerLayout and toolbar to MainActivity.
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener{
+
+    private DrawerLayout drawerLayout;
+    private DrawerAdapter drawerAdapter;
+
+    private FragmentManager fragmentManager;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
 
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        android.support.design.widget.TabLayout tabLayout = findViewById(R.id.tab_layout);
+        createUI();
+    }
 
-        Log.i("Error", "!!!!!!!!!" + tabLayout.toString() + "!!!!!!!!!!!!!!!!");
+    public void createUI(){
 
-        tabLayout.addTab(tabLayout.newTab().setText(R.string.tab_profile), 0);
-        tabLayout.addTab(tabLayout.newTab().setText(R.string.tab_map), 1);
-        tabLayout.addTab(tabLayout.newTab().setText(R.string.tab_game), 2);
-        tabLayout.addTab(tabLayout.newTab().setText(R.string.tab_settings), 3);
+        drawerAdapter = new DrawerAdapter();        //DrawerAdapter instanties fragments
+        drawerLayout = (DrawerLayout) findViewById(R.id.drawer_layout);
 
-        tabLayout.setTabGravity(TabLayout.GRAVITY_FILL);
+        //This handles all our fragments
+        fragmentManager = getSupportFragmentManager();
 
-        final ViewPager viewPager = (ViewPager) findViewById(R.id.pager);
-        final PagerAdapter adapter = new PagerAdapter(getSupportFragmentManager(), tabLayout.getTabCount());
 
-        viewPager.setAdapter(adapter);
+        //This is toolbar on top of screen.
+        Toolbar myToolBar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(myToolBar);
 
-        viewPager.addOnPageChangeListener(new TabLayout.TabLayoutOnPageChangeListener(tabLayout));
+        //Navigation buttons
+        Button loginButton = (Button) findViewById(R.id.button_login);
+        ImageButton settingsButton = (ImageButton) findViewById(R.id.button_settings);
+        Button mapButton = (Button) findViewById(R.id.toolbar_map_button);
+        Button partnersButton = (Button) findViewById(R.id.toolbar_partners_button);
 
-        tabLayout.addOnTabSelectedListener(new TabLayout.OnTabSelectedListener() {
+        //Add click listeners to all buttons
+        loginButton.setOnClickListener(this);
+        settingsButton.setOnClickListener(this);
+        mapButton.setOnClickListener(this);
+        partnersButton.setOnClickListener(this);
+
+        //Add drawer toggle button and listener for it.
+        ActionBarDrawerToggle abToggle = new ActionBarDrawerToggle(
+                this, drawerLayout, myToolBar, R.string.actionbar_drawer_open, R.string.actionbar_drawer_close);
+        drawerLayout.addDrawerListener(abToggle);
+        abToggle.setToolbarNavigationClickListener(new View.OnClickListener(){
             @Override
-            public void onTabSelected(TabLayout.Tab tab) {
-                viewPager.setCurrentItem(tab.getPosition());
-            }
-
-            @Override
-            public void onTabUnselected(TabLayout.Tab tab) {
-
-            }
-
-            @Override
-            public void onTabReselected(TabLayout.Tab tab) {
-
+            public void onClick(View view){
+                drawerLayout.openDrawer(GravityCompat.START);
             }
         });
 
-        viewPager.setCurrentItem(1);
+        //Set default drawerToggleButton to false, and then replace it with custom icon.
+        abToggle.setDrawerIndicatorEnabled(false);
+        abToggle.setHomeAsUpIndicator(R.drawable.ic_person_blue);
+        abToggle.syncState();
+
+        //Insert map fragment to main_content view.
+        fragmentManager.beginTransaction()
+                .replace(R.id.main_content, drawerAdapter.getItem(0))
+                .commit();
+    }
+
+    //Handle button click events here.
+    @Override
+    public void onClick(View view){
+        switch(view.getId()){
+            case R.id.button_settings:
+                fragmentManager.beginTransaction()
+                        .replace(R.id.main_content, drawerAdapter.getItem(1))
+                        .commit();
+             //   myTitleView.setText(R.string.tab_settings);
+                drawerLayout.closeDrawer(GravityCompat.START);
+                break;
+            case R.id.button_login:
+                drawerLayout.closeDrawer(GravityCompat.START);
+                Toast.makeText(MainActivity.this, "Clicked login", Toast.LENGTH_SHORT).show();
+                break;
+            case R.id.finnair_logo_button:
+                fragmentManager.beginTransaction()
+                        .replace(R.id.main_content, drawerAdapter.getItem(0))
+                        .commit();
+            //    myTitleView.setText(R.string.tab_map);
+                break;
+            case R.id.toolbar_map_button:
+                fragmentManager.beginTransaction()
+                        .replace(R.id.main_content, drawerAdapter.getItem(0))
+                        .commit();
+                break;
+            case R.id.toolbar_partners_button:
+                Toast.makeText(MainActivity.this, "Open partner list", Toast.LENGTH_SHORT).show();
+            default: break;
+        }
     }
 
 
 }
+
